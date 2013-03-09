4ac2e33ba179e66e685482ec69e8e9481f0889da
Harald Zealot <harald_zealot@tut.by>
2013-03-09 14:56:45 +0300

    prepare for dynamic matrix

--- Snakelib.h
+++ Snakelib.h
@@ -1,23 +1,24 @@
 #ifndef SNAKELIB_H
 #define SNAKELIB_H
 
-const short M=16, N=51, start_len=(M-2)*(N-2);
-const char cell=' ', snake_icon='O', food_icon='+', border_icon='#';
+const short M = 16, N = 51, start_len = (M - 2) * (N - 2);
+const char cell = ' ', snake_icon = 'O', food_icon = '+', border_icon = '#';
 
 struct Symbol
 {
-    short x;
-    short y;
+   short x;
+   short y;
 };
 
-void input(char array[M][N]);
-void draw(char array[M][N]);
+void input (char array[M][N], int M, int N);
+void draw (char array[M][N], int M, int N);
 void clear();
-void move(char array[M][N], Symbol *snake, short n);
-void snake_in(char array[M][N], Symbol *snake, short n);
-void item_in(char array[M][N]);
+void move (char array[M][N], int M, int N, Symbol *snake, short n);
+void snake_in (char array[M][N], int M, int N, Symbol *snake, short n);
+void item_in (char array[M][N], int M, int N);
 int kbhit();
 int getch();
 
 #endif
+
 
--- main.cpp
+++ main.cpp
@@ -10,86 +10,106 @@ using namespace std;
 
 int main()
 {
-    srand(time(NULL));
-    Symbol food, *snake=NULL;
-    snake=new Symbol[start_len];
-    short len=3;
-    char field[M][N];
-    input(field);
-    snake_in(field, snake, len);
-    //item_in(field, food);
-    do
-    {
-        food.x=rand()%(M-2)+1;
-        food.y=rand()%(N-2)+1;
-    }
-    while (field[food.x][food.y]==snake_icon);
-    field[food.x][food.y]=food_icon;
+   srand (time (NULL));
+   Symbol food, *snake = NULL;
+   snake = new Symbol[start_len];
+   short len = 3;
+   char field[M][N];
+   input (field, M, N);
+   snake_in (field, M, N, snake, len);
 
-    draw(field);
-    char button='d', twin=button;
-    bool game=true;
-    short score=0;
-    while(game)
-    {
-        if(kbhit())
-            button=getch();
-        move(field, snake, len);
-        if ((button=='s'&&twin!='w')||(button=='w'&&twin!='s')||(button=='a'&&twin!='d')||(button=='d'&&twin!='a'))
-            twin=button;
-        switch (twin)
-        {
-        case 'w':
-            usleep(145000);
+   //item_in(field, food);
+   do
+   {
+      food.x = rand() % (M - 2) + 1;
+      food.y = rand() % (N - 2) + 1;
+   }
+   while (field[food.x][food.y] == snake_icon);
+
+   field[food.x][food.y] = food_icon;
+
+   draw (field, M, N);
+   char button = 'd', twin = button;
+   bool game = true;
+   short score = 0;
+
+   while (game)
+   {
+      if (kbhit())
+         button = getch();
+
+      move (field, M, N , snake, len);
+
+      if ( (button == 's' && twin != 'w') || (button == 'w' && twin != 's') || (button == 'a' && twin != 'd') || (button == 'd' && twin != 'a'))
+         twin = button;
+
+      switch (twin)
+      {
+         case 'w':
+            usleep (145000);
             snake[len-1].x--;
-            if (snake[len-1].x<1)
-                snake[len-1].x=M-2;
+
+            if (snake[len-1].x < 1)
+               snake[len-1].x = M - 2;
+
             break;
-        case 's':
-            usleep(145000);
+         case 's':
+            usleep (145000);
             snake[len-1].x++;
-            if (snake[len-1].x>M-2)
-                snake[len-1].x=1;
+
+            if (snake[len-1].x > M - 2)
+               snake[len-1].x = 1;
+
             break;
-        case 'a':
-            usleep(100000);
+         case 'a':
+            usleep (100000);
             snake[len-1].y--;
-            if (snake[len-1].y<1)
-                snake[len-1].y=N-2;
+
+            if (snake[len-1].y < 1)
+               snake[len-1].y = N - 2;
+
             break;
-        case 'd':
-            usleep(100000);
+         case 'd':
+            usleep (100000);
             snake[len-1].y++;
-            if (snake[len-1].y>N-2)
-                snake[len-1].y=1;
+
+            if (snake[len-1].y > N - 2)
+               snake[len-1].y = 1;
+
             break;
-        }
-        if (field[snake[len-1].x][snake[len-1].y]==snake_icon)
-        {
-            game=false;
-            cout<<endl<<"Счёт: "<<score<<endl;
-            delete[]snake;
-            sleep(1);
-            continue;
-        }
-        field[snake[len-1].x][snake[len-1].y]=snake_icon;
-        clear();
-        draw(field);
-        if (field[snake[len-1].x][snake[len-1].y]==field[food.x][food.y])
-        {
-            len++;
-            score++;
-            snake[len-1].x=snake[len-2].x;
-            snake[len-1].y=snake[len-2].y;
-            //item_in(field, food);
-            do
-            {
-                food.x=rand()%(M-2)+1;
-                food.y=rand()%(N-2)+1;
-            }
-            while (field[food.x][food.y]==snake_icon);
-            field[food.x][food.y]=food_icon;
-        }
-    }
-    return 0;
+      }
+
+      if (field[snake[len-1].x][snake[len-1].y] == snake_icon)
+      {
+         game = false;
+         cout << endl << "Счёт: " << score << endl;
+         delete[]snake;
+         sleep (1);
+         continue;
+      }
+
+      field[snake[len-1].x][snake[len-1].y] = snake_icon;
+      clear();
+      draw (field, M, N);
+
+      if (field[snake[len-1].x][snake[len-1].y] == field[food.x][food.y])
+      {
+         len++;
+         score++;
+         snake[len-1].x = snake[len-2].x;
+         snake[len-1].y = snake[len-2].y;
+
+         //item_in(field, food);
+         do
+         {
+            food.x = rand() % (M - 2) + 1;
+            food.y = rand() % (N - 2) + 1;
+         }
+         while (field[food.x][food.y] == snake_icon);
+
+         field[food.x][food.y] = food_icon;
+      }
+   }
+
+   return 0;
 }
